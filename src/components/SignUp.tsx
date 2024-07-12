7c80c65beb5c02e3ac97e79dee7c8771965edec8
ArturLobur <loburav@gmail.com>
2024-07-11 20:55:06 -0700

    feat: sign-up fix autoFocus

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -64,7 +64,6 @@ export default function SignUp() {
           label="Email Address"
           name="email"
           autoComplete="email"
-          autoFocus
         />
         <TextField
           margin="normal"
