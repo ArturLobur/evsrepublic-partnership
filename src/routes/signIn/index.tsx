0d39304a5db22489fb9d74228d0e6d95576938cd
ArturLobur <loburav@gmail.com>
2024-07-22 01:16:11 -0700

    feat: fix err 2

--- src/routes/signIn/index.tsx
+++
@@ -1,7 +0,0 @@
-import {createFileRoute} from "@tanstack/react-router";
-
-import SignInSide from "../../components/Auth/SignIn.tsx";
-
-export const Route = createFileRoute("/signIn/")({
-  component: () => <SignInSide />,
-});
