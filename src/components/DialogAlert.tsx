f38a9ef1a2bacfa8d8742f79e710b5142fc174a0
ArturLobur <loburav@gmail.com>
2024-07-23 23:52:02 -0700

    feat: add social icons

--- src/components/DialogAlert.tsx
+++ src/components/DialogAlert.tsx
@@ -1,7 +1,12 @@
-import {DialogActions, DialogContent} from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import {DialogActions, DialogContent, IconButton} from "@mui/material";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
+import Stack from "@mui/material/Stack";
 
 import {useDialog} from "../store/DialogContext.tsx";
 
@@ -18,6 +23,55 @@ const DialogAlert = () => {
         Partners EV’s Republic. Our team will be reaching out to you via phone
         during the next week to provide further assistance and answer any
         questions you may have.
+      </DialogContent>
+      <DialogContent>
+        Stay Updated
+        <br />
+        Follow us on social media for the latest news, updates, and exclusive
+        content:
+        <Stack
+          direction="row"
+          justifyContent="center"
+          spacing={1}
+          useFlexGap
+          sx={{
+            mt: 2,
+            color: "text.secondary",
+          }}
+        >
+          <IconButton
+            color="inherit"
+            href="https://www.facebook.com/EVSRepublic"
+            aria-label="facebook"
+            sx={{alignSelf: "center"}}
+          >
+            <FacebookIcon />
+          </IconButton>
+          <IconButton
+            color="inherit"
+            href="https://www.twitter.com/EVSRepublic"
+            aria-label="twitter"
+            sx={{alignSelf: "center"}}
+          >
+            <TwitterIcon />
+          </IconButton>
+          <IconButton
+            color="inherit"
+            href="https://www.linkedin.com/company/EVSRepublic"
+            aria-label="LinkedIn"
+            sx={{alignSelf: "center"}}
+          >
+            <LinkedInIcon />
+          </IconButton>
+          <IconButton
+            color="inherit"
+            href="https://www.instagram.com/EVSRepublic"
+            aria-label="instagram"
+            sx={{alignSelf: "center"}}
+          >
+            <InstagramIcon />
+          </IconButton>
+        </Stack>
       </DialogContent>
       <DialogActions>
         <Button onClick={toggleDialog}>Close</Button>
