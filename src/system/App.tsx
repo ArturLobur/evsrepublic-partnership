e43da9202f1054614b9a00cd0b61c5c554e22468
ArturLobur <loburav@gmail.com>
2024-09-09 10:32:41 -0700

    delete routing

--- src/system/App.tsx
+++ src/system/App.tsx
@@ -1,36 +1,33 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import {createTheme} from "@mui/material";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import {ThemeProvider} from "@mui/material/styles";
-import {createFileRoute} from "@tanstack/react-router";
 
 import About from "../components/About.tsx";
 import AppAppBar from "../components/AppAppBar.tsx";
 import CoursesAndCertifications from "../components/CoursesAndCertifications.tsx";
 import DialogAlert from "../components/DialogAlert.tsx";
 import Footer from "../components/Footer.tsx";
+import HowItWorks from "../components/HowItWorks.tsx";
 import LaborTimes from "../components/LaborTimes.tsx";
 import Marketing from "../components/Marketing.tsx";
 import Service from "../components/Service.tsx";
 import {DialogProvider} from "../store/DialogContext.tsx";
-import {siteColors} from "../system/constants.ts";
-import HowItWorks from "../components/HowItWorks.tsx";
+import {siteColors} from "./constants.ts";
 
-export const Route = createFileRoute("/")({
-  component: LandingPage,
-});
-
-function LandingPage() {
+const App = () => {
   const theme = createTheme({
     palette: {
       mode: "dark",
       primary: {
         main: siteColors.neon,
       },
-    },
-    typography: {
-      fontFamily: ["Montserrat", "sans-serif"].join(","),
     },
   });
 
@@ -52,11 +49,13 @@ function LandingPage() {
 
           <Marketing />
           <Divider />
-          <HowItWorks/>
+          <HowItWorks />
           <Footer />
         </Box>
         <DialogAlert />
       </DialogProvider>
     </ThemeProvider>
   );
-}
+};
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import {TanStackRouterVite} from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import path from "path";
 import {defineConfig} from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), viteReact()],
+  plugins: [viteReact()],
   build: {
     outDir: path.join(__dirname, "build"),
   },
