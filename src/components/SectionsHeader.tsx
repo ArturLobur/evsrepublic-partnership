f9d8cec4d0de0aaa585e4a1013f9a5a4a307a590
ArturLobur <loburav@gmail.com>
2024-09-24 19:19:26 -0700

    layout: create SectionHeader component

---
+++ src/components/SectionsHeader.tsx
@@ -0,0 +1,47 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import React from "react";
+
+interface SectionsHeaderPropsI {
+  title: string;
+  firstDescription: string;
+  secondDescription: string;
+  children: React.ReactNode;
+}
+
+const SectionsHeader: React.FC<SectionsHeaderPropsI> = ({
+  title,
+  firstDescription,
+  secondDescription,
+  children,
+}) => (
+  <Grid container spacing={4} sx={{pt: 12, mb: 12}}>
+    <Grid item xs={12}>
+      <Typography
+        variant="h3"
+        color="primary"
+        sx={{fontSize: 48, lineHeight: "51px", fontWeight: 400}}
+      >
+        {title}
+      </Typography>
+    </Grid>
+    <Grid item xs={6}>
+      <Typography sx={{fontSize: 26, lineHeight: "36px", fontWeight: 400}}>
+        {firstDescription}
+      </Typography>
+    </Grid>
+    <Grid item xs={6} mb={7.5}>
+      <Typography
+        color="text.secondary"
+        sx={{fontSize: 16, lineHeight: "24px", fontWeight: 400}}
+      >
+        {secondDescription}
+      </Typography>
+    </Grid>
+    <Grid item xs={12}>
+      {children}
+    </Grid>
+  </Grid>
+);
+
+export default SectionsHeader;
--- src/sections/SectionRemoteServices/SectionRemoteServices.tsx
+++ src/sections/SectionRemoteServices/SectionRemoteServices.tsx
@@ -1,11 +1,11 @@
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 
 import ConsultingIcon from "../../assets/consultingIcon.svg";
 import DiagnosticIcon from "../../assets/diagnosticsIcon.svg";
 import ProgrammingIcon from "../../assets/programmingIcon.svg";
 import CardItem from "../../components/CardItem.tsx";
+import SectionsHeader from "../../components/SectionsHeader.tsx";
 
 const operations = [
   {
@@ -30,54 +30,34 @@ const operations = [
 
 const SectionRemoteServices = () => (
   <Container maxWidth="xl">
-    <Grid container spacing={4} sx={{pt: 12, mb: 12}}>
-      <Grid item xs={12}>
-        <Typography
-          variant="h3"
-          color="primary"
-          sx={{fontSize: 48, lineHeight: "51px", fontWeight: 400}}
-        >
-          Remote Services
-        </Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <Typography sx={{fontSize: 26, lineHeight: "36px", fontWeight: 400}}>
-          Enhance Your Operations with Advanced Remote Services.
-        </Typography>
-      </Grid>
-      <Grid item xs={6} mb={7.5}>
-        <Typography
-          color="text.secondary"
-          sx={{fontSize: 16, lineHeight: "24px", fontWeight: 400}}
-        >
-          Knowledge is power, especially in the fast-evolving world of electric
+    <SectionsHeader
+      title="Remote Services"
+      firstDescription="Enhance Your Operations with Advanced Remote Services."
+      secondDescription="Knowledge is power, especially in the fast-evolving world of electric
           vehicles. At our company we provide access to high-quality training
           courses and certification programs designed to equip you and your
-          staff with the skills needed to excel in the EV industry.
-        </Typography>
+          staff with the skills needed to excel in the EV industry."
+    >
+      <Grid
+        container
+        direction="row"
+        sx={{
+          justifyContent: "space-between",
+          alignItems: "stretch",
+        }}
+      >
+        {operations.map((item) => (
+          <CardItem
+            key={item.title}
+            variant="elevation"
+            icon={item.icon}
+            title={item.title}
+            description={item.description}
+            backgroundColor="rgba(10, 10, 17, 0.3)"
+          />
+        ))}
       </Grid>
-      <Grid item xs={12}>
-        <Grid
-          container
-          direction="row"
-          sx={{
-            justifyContent: "space-between",
-            alignItems: "stretch",
-          }}
-        >
-          {operations.map((item) => (
-            <CardItem
-              key={item.title}
-              variant="elevation"
-              icon={item.icon}
-              title={item.title}
-              description={item.description}
-              backgroundColor="rgba(10, 10, 17, 0.3)"
-            />
-          ))}
-        </Grid>
-      </Grid>
-    </Grid>
+    </SectionsHeader>
   </Container>
 );
 export default SectionRemoteServices;
