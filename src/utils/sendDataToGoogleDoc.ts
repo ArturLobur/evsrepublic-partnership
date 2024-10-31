91c2f56fb484547691617d42755c8ec40e507dcb
ArturLobur <loburav@gmail.com>
2024-10-30 22:23:20 -0700

    fix: change link 2

--- src/utils/sendDataToGoogleDoc.ts
+++ src/utils/sendDataToGoogleDoc.ts
@@ -2,7 +2,7 @@ import toast from "react-hot-toast";
 
 export async function sendDataToGoogleDoc(data: FormData) {
   fetch(
-    "https://script.google.com/macros/s/AKfycbz-NW064vhgsnX9Y4mW35qdUa8MCJRkFbO0phZYS2x3NLt4oGucFpNkmnol001R1-YFEw/exec",
+    "https://script.google.com/macros/s/AKfycbyO5ehQNOTbe4tCBe1bRjuWiNJuF6RqNszJtHFIejmRGTzhl_JFJTTvmxphSEEwaTteNg/exec",
     {
       method: "POST",
       body: data,
