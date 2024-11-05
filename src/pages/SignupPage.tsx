31283dd5aa48384b976f04626942835d85ac055e
ArturLobur <loburav@gmail.com>
2024-11-04 18:22:45 -0800

    feat: add new page

---
+++ src/pages/SignupPage.tsx
@@ -0,0 +1,169 @@
+import PersonAddOutlinedIcon from "@mui/icons-material/PersonAddOutlined";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+
+import logo from "../assets/logo.png";
+import {useDialog} from "../store/DialogContext.tsx";
+import {averageRepairs, states} from "../system/constants.ts";
+import {sendDataToGoogleDoc} from "../utils/sendDataToGoogleDoc.ts";
+
+const logoStyle = {
+  padding: "20px 0",
+  width: "133px",
+  height: "auto",
+  cursor: "pointer",
+  marginTop: "-12px",
+};
+
+const SignupPage = () => {
+  const {toggleDialog} = useDialog();
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    sendDataToGoogleDoc(data).then(() => toggleDialog());
+    (event.target as HTMLFormElement).reset();
+  };
+  return (
+    <Box
+      sx={{
+        p: 4,
+        overflowY: "auto",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <img id="logo" src={logo} style={logoStyle} alt="logo" />
+      <Avatar sx={{m: 1}}>
+        <PersonAddOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign up as a new partner
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} sx={{mt: 1}}>
+        <TextField
+          margin="normal"
+          autoComplete="given-name"
+          name="FirstName"
+          variant="outlined"
+          required
+          fullWidth
+          id="firstName"
+          label="First Name"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          variant="outlined"
+          required
+          fullWidth
+          id="lastName"
+          label="Last Name"
+          name="LastName"
+          autoComplete="family-name"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="Email"
+          autoComplete="email"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="Telephone"
+          label="Phone"
+          type="tel"
+          id="tel"
+          autoComplete="tel"
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          name="companyName"
+          label="Company Name"
+          type="text"
+          id="companyName"
+        />
+        <TextField
+          margin="normal"
+          select
+          fullWidth
+          name="averageRepairs"
+          label="Average Monthly Requests for Tesla Repairs"
+          type="text"
+          id="averageRepairs"
+        >
+          {averageRepairs.map((repairs) => (
+            <MenuItem key={repairs.value} value={repairs.value}>
+              {repairs.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          margin="normal"
+          fullWidth
+          name="country"
+          label="Country"
+          type="text"
+          id="country"
+          defaultValue="United States"
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          select
+          name="state"
+          label="State"
+          id="state"
+        >
+          {states.map((state) => (
+            <MenuItem key={state.value} value={state.value}>
+              {state.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          margin="normal"
+          fullWidth
+          name="city"
+          label="City"
+          type="text"
+          id="city"
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          name="address"
+          label="Address"
+          type="text"
+          id="address"
+          placeholder="Example: 1234 Main St, Suite 210"
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          name="years"
+          label="Years in Operation"
+          type="text"
+          id="years"
+        />
+
+        <Button type="submit" fullWidth variant="contained" sx={{mt: 3, mb: 2}}>
+          Sign Up
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default SignupPage;
